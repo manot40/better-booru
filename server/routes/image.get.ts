2f318dfe8180c1c1c914d92d272764cecc686aae
manot40 <kevinsandiho290100@gmail.com>
2025-02-08 15:42:26 +0700

    Fix: adjust caching

--- server/routes/image.get.ts
+++ server/routes/image.get.ts
@@ -1,6 +1,17 @@
-export default defineEventHandler((evt) => {
+export default defineEventHandler(async (evt) => {
   const { proxy } = getQuery(evt);
   if (typeof proxy != 'string') return createError({ statusCode: 404 });
   if (!proxy.includes('booru')) return createError({ statusCode: 400 });
-  return fetch(proxy);
+
+  const res = await fetch(proxy);
+  if (!res.ok)
+    return createError({
+      data: await res.text(),
+      statusCode: res.status,
+      statusText: res.statusText,
+    });
+
+  setResponseHeader(evt, 'content-type', res.headers.get('content-type') || 'application/octet-stream');
+  setResponseHeader(evt, 'Cache-Control', 'public, max-age=31536000');
+  return res.arrayBuffer();
 });
