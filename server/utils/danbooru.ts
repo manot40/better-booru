52cac7cc22de6f6874cb76dc5cd03c536dcc5ca0
manot40 <kevinsandiho290100@gmail.com>
2025-02-17 12:52:19 +0700

    Fix: low quality image

--- server/utils/danbooru.ts
+++ server/utils/danbooru.ts
@@ -17,30 +17,31 @@ export const convertDanbooruRating = (data: DanbooruResponse['rating']): Rating 
   }
 };
 
-export const getDanbooruImage = (data: DanbooruResponse) =>
-  data.media_asset.variants.reduce(
-    (acc, next) => {
-      if (next.type === 'original') {
-        acc.width = next.width;
-        acc.height = next.height;
-        acc.file_url = next.url;
-      } else if (next.type === 'sample') {
-        acc.sample_url = next.url;
-        acc.sample_width = next.width;
-        acc.sample_height = next.height;
-      } else if (next.type === '720x720') {
-        acc.preview_url = next.url;
-        acc.preview_width = next.width;
-        acc.preview_height = next.height;
-      } else if (!acc.sample_url && !acc.sample_width && !acc.sample_height) {
-        acc.sample_url = next.url;
-        acc.sample_width = next.width;
-        acc.sample_height = next.height;
+export const getDanbooruImage = (data: DanbooruResponse) => {
+  const extracted = <ExtractedImage>{};
+  const hasSample = data.media_asset.variants.some((v) => v.type === 'sample');
+  return data.media_asset.variants.reduce((acc, next) => {
+    if (next.type === 'original') {
+      const s = (acc.file_url = next.url);
+      const w = (acc.width = next.width);
+      const h = (acc.height = next.height);
+      if (!hasSample) {
+        acc.sample_url = s;
+        acc.sample_width = w;
+        acc.sample_height = h;
       }
-      return acc;
-    },
-    <ExtractedImage>{}
-  );
+    } else if (next.type === 'sample') {
+      acc.sample_url = next.url;
+      acc.sample_width = next.width;
+      acc.sample_height = next.height;
+    } else if (next.type === '720x720') {
+      acc.preview_url = next.url;
+      acc.preview_width = next.width;
+      acc.preview_height = next.height;
+    }
+    return acc;
+  }, extracted);
+};
 
 type ExtractedImage = Pick<
   Post,
