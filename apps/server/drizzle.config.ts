437aec1c3afa3c75b05295184480905eba06f43a
manot40 <kevinsandiho290100@gmail.com>
2025-04-05 16:44:19 +0700

    Fix: schema

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './server/db/schema.ts',
+  schema: './src/db/schema.ts',
   dialect: 'sqlite',
   dbCredentials: { url: `file:${process.env.DB_FILE_PATH!}` },
 });
--- apps/server/src/handlers/autocomplete.ts
+++ apps/server/src/handlers/autocomplete.ts
@@ -5,6 +5,10 @@ import { type InferHandler, t } from 'elysia';
 import { $danbooruFetch, $gelbooruFetch } from 'utils/fetcher';
 
 type Autocomplete = typeof autocompleteSchema.static;
+type GelAutocomplete = Omit<Autocomplete, 'category' | 'post_count'> & {
+  category: string;
+  post_count: string;
+};
 const autocompleteSchema = t.Object({
   label: t.String(),
   value: t.String(),
@@ -17,10 +21,10 @@ export const handler: Handler = async ({ query, userConfig, headers }) => {
   const provider = qProvider || headers['x-provider'] || userConfig?.provider;
 
   if (provider === 'gelbooru') {
-    const data = await $gelbooruFetch<Autocomplete[]>('/index.php', {
+    const data = await $gelbooruFetch<GelAutocomplete[]>('/index.php', {
       query: { page: 'autocomplete2', term: q, type: 'tag_query', limit: '10' },
     });
-    return data;
+    return data.map((d) => ({ ...d, category: booruCategory(d.category), post_count: +d.post_count }));
   } else {
     const data = await $danbooruFetch<Autocomplete[]>('/autocomplete.json', {
       query: { limit: '20', version: '1', 'search[type]': 'tag_query', 'search[query]': q },
@@ -35,3 +39,18 @@ type Handler = InferHandler<Setup, '/api/autocomplete', Schema>;
 const query = t.Object({ q: t.String(), provider: t.Optional(t.UnionEnum(['danbooru', 'gelbooru'])) });
 const response = t.Array(autocompleteSchema);
 export const schema = { query, response };
+
+function booruCategory(cat: string): Autocomplete['category'] {
+  switch (cat) {
+    case 'meta':
+      return 5;
+    case 'character':
+      return 4;
+    case 'copyright':
+      return 3;
+    case 'artist':
+      return 1;
+    default:
+      return 0;
+  }
+}
