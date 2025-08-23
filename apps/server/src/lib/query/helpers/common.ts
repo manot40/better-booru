a4dc9a1f2f9a638ea66e879f2884db1f11e81e31
manot40 <kevinsandiho290100@gmail.com>
2025-08-24 00:07:57 +0700

    Fix: tags ordering

--- apps/server/src/lib/query/helpers/common.ts
+++ apps/server/src/lib/query/helpers/common.ts
@@ -1,25 +1,25 @@
-import { schema as $s, type Transaction } from 'db';
-
-import { inArray } from 'drizzle-orm';
+import type { Transaction } from 'db';
 
 export type DeserializedTags = Record<'eq' | 'ne', number[] | undefined>;
 
 export async function deserializeTags(tx: Transaction, tags: string[]): Promise<DeserializedTags> {
-  const normalized = tags.map((t) => {
-    const exc = /^-/.test(t);
-    const tagName = exc ? t.slice(1) : t;
-    return { exc, tagName };
-  });
+  const normalized = tags
+    .map((t) => {
+      const exc = /^-/.test(t);
+      const tagName = exc ? t.slice(1) : t;
+      return { exc, tagName };
+    })
+    .sort((a, b) => a.tagName.localeCompare(b.tagName));
 
-  const result = await tx
-    .select()
-    .from($s.tagsTable)
-    .where(
-      inArray(
-        $s.tagsTable.name,
-        normalized.map((t) => t.tagName)
-      )
-    )
+  const result = await tx.query.tagsTable
+    .findMany({
+      where: (table, { inArray }) =>
+        inArray(
+          table.name,
+          normalized.map((t) => t.tagName)
+        ),
+      orderBy: (table, { asc }) => asc(table.name),
+    })
     .then((res) => res.map((tag, i) => ({ tag, exc: normalized[i].exc })));
 
   return result.reduce(
