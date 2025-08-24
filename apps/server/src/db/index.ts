27efb594f97f2a936de75669e034b0d11f74e77e
manot40 <kevinsandiho290100@gmail.com>
2025-08-24 15:41:23 +0700

    Fix: revert back to bun sql

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,10 +1,10 @@
-import type { Sql } from 'postgres';
+import type { SQL } from 'bun';
 import type { PgTransaction } from 'drizzle-orm/pg-core';
 import type { ExtractTablesWithRelations } from 'drizzle-orm';
-import type { PostgresJsQueryResultHKT, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import type { BunSQLQueryResultHKT as ResultHKT, BunSQLDatabase as DB } from 'drizzle-orm/bun-sql';
 
 import * as schema from './schema';
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle } from 'drizzle-orm/bun-sql';
 
 const DB_URL = Bun.env.DATABASE_URL || 'noop';
 
@@ -12,9 +12,5 @@ const db = <Database>drizzle(DB_URL, { schema });
 db.enabled = !!DB_URL || DB_URL !== 'noop';
 
 export { db, schema };
-export type Database = PostgresJsDatabase<typeof schema> & { $client: Sql<{}>; enabled: boolean };
-export type Transaction = PgTransaction<
-  PostgresJsQueryResultHKT,
-  typeof schema,
-  ExtractTablesWithRelations<typeof schema>
->;
+export type Database = DB<typeof schema> & { $client: SQL; enabled: boolean };
+export type Transaction = PgTransaction<ResultHKT, typeof schema, ExtractTablesWithRelations<typeof schema>>;
--- apps/server/src/worker.ts
+++ apps/server/src/worker.ts
@@ -7,7 +7,7 @@ declare var self: Worker;
 self.addEventListener('message', async (event) => {
   const evData = <WorkerEventPayload>parsePayload(event);
   if (evData.type === 'QueryPosts') self.postMessage(await queryPosts(evData.payload));
-  db.$client.end();
+  db.$client.end({ timeout: 1 });
 });
 
 function parsePayload(ev: MessageEvent<any>) {
