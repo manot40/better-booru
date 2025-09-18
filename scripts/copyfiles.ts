889ed13872fd988c539aa75b59c6e32c72211f4b
manot40 <kevinsandiho290100@gmail.com>
2025-09-18 21:43:06 +0700

    Fix: revert old copyfile style

--- scripts/copyfiles.ts
+++ scripts/copyfiles.ts
@@ -1,5 +1,5 @@
 import { join, basename } from 'node:path';
-import { cpSync, existsSync, mkdirSync, rmSync } from 'node:fs';
+import { cpSync, existsSync, rmSync } from 'node:fs';
 
 import { S3_ENABLED, NUXT_DIR } from './common';
 
@@ -15,37 +15,28 @@ if (existsSync(DIST_DIR)) {
 cpSync('./apps/server/dist', DIST_DIR, { recursive: true });
 cpSync(join(process.cwd(), SHARP_MOD), join(DIST_DIR, SHARP_MOD), { recursive: true });
 
-/**
- * Copy frontend build files:
- * Static assets will be uploaded to CDN (if enabled),
- * so they are not copied to the dist directory.
- */
-if (!S3_ENABLED) {
-  const assetsGlob = new Bun.Glob(join(process.cwd(), ARTIFACTS));
+/** Copy frontend build files */
+const assetsGlob = new Bun.Glob(join(process.cwd(), ARTIFACTS));
+for await (const filePath of assetsGlob.scan()) {
+  const fileUrl = Bun.pathToFileURL(filePath).toString();
+  const isNuxtFile = fileUrl.includes(`${NUXT_DIR}/`);
 
-  for await (const filePath of assetsGlob.scan()) {
-    const fileUrl = Bun.pathToFileURL(filePath).toString();
-    const isNuxtFile = fileUrl.includes(`${NUXT_DIR}/`);
+  /**
+   * Nuxt static assets will be uploaded to CDN (if enabled),
+   * so they are not copied to the dist directory.
+   */
+  if (S3_ENABLED && isNuxtFile) continue;
 
-    let destination: string;
-    if (isNuxtFile) {
-      const fileName = fileUrl.replace(/.*\/.output\//g, '');
-      destination = join(DIST_DIR, fileName);
-    } else {
-      const fileName = basename(filePath);
-      destination = join(DIST_DIR, 'public', fileName);
-    }
-
-    cpSync(filePath, destination, { recursive: true });
+  let destination: string;
+  if (isNuxtFile) {
+    const fileName = fileUrl.replace(/.*\/.output\//g, '');
+    destination = join(DIST_DIR, fileName);
+  } else {
+    const fileName = basename(filePath);
+    destination = join(DIST_DIR, 'public', fileName);
   }
-} else {
-  const PUBLIC_DIR = join(DIST_DIR, 'public');
-  const NOT_FOUND_CONTENT =
-    '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Not Found</title></head><body>404 Not Found</body></html>';
 
-  mkdirSync(PUBLIC_DIR);
-  await Bun.file(join(PUBLIC_DIR, '404.html')).write(NOT_FOUND_CONTENT);
-  await Bun.file(join(PUBLIC_DIR, 'index.html')).write(NOT_FOUND_CONTENT);
+  cpSync(filePath, destination, { recursive: true });
 }
 
 console.info(Bun.color('green', 'ansi'), 'Build artifacts successfully prepared.');
