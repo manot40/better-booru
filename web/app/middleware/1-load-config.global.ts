227ac88c9e9b754089b24c4c2cb9711697eeef44
manot40 <kevinsandiho290100@gmail.com>
2026-08-20 18:48:42 +0700

    Fix (web): user-config not sent

--- web/app/middleware/1-load-config.global.ts
+++ web/app/middleware/1-load-config.global.ts
@@ -1,5 +1,3 @@
-import type { $Fetch } from 'ofetch';
-
 import { client } from '~/api/client.gen';
 
 export default defineNuxtRouteMiddleware(() => {
@@ -13,17 +11,15 @@ export default defineNuxtRouteMiddleware(() => {
 
   client.setConfig({
     baseUrl: runConfig.public.baseUrl || (typeof location != 'undefined' ? location.origin : fallback),
-    ofetch: $fetch.create({
-      onRequest({ options }) {
-        if (typeof options.headers?.set !== 'function') {
-          options.headers = new Headers({ ...options.headers });
-        }
+    onRequest({ options }) {
+      if (typeof options.headers?.set !== 'function') {
+        options.headers = new Headers({ ...options.headers });
+      }
 
-        if (import.meta.client) {
-          const configStr = localStorage[STATIC.keys.userConfig];
-          if (configStr) options.headers.set(`x-${STATIC.keys.userConfig}`, btoa(configStr));
-        }
-      },
-    }) as $Fetch,
+      if (import.meta.client) {
+        const configStr = localStorage[STATIC.keys.userConfig];
+        if (configStr) options.headers.set(`x-${STATIC.keys.userConfig}`, btoa(configStr));
+      }
+    },
   });
 });
